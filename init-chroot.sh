6188f369a3cb2a33707328957f6c7eb875a48c44
Adrien Thebo <git@somethingsinistral.net>
2014-05-05 11:02:58 -0700

    reindent

--- init-chroot.sh
+++ init-chroot.sh
@@ -6,112 +6,119 @@ export MIRROR="http://gentoo.osuosl.org"
 GENTOO_BUILD="20140410"
 
 
-function download_stage3() {
+download_stage3() {
 
-        STAGE3="stage3-amd64-${GENTOO_BUILD}.tar.bz2"
-        STAGE3_CHECKSUM="stage3-amd64-${GENTOO_BUILD}.tar.bz2.DIGESTS"
+    STAGE3="stage3-amd64-${GENTOO_BUILD}.tar.bz2"
+    STAGE3_CHECKSUM="stage3-amd64-${GENTOO_BUILD}.tar.bz2.DIGESTS"
 
-        BUILD_URL="${MIRROR}/releases/amd64/autobuilds/current-stage3-amd64"
-        STAGE3_URL="${BUILD_URL}/${STAGE3}"
-        STAGE3_CHECKSUM_URL="${BUILD_URL}/${STAGE3_CHECKSUM}"
+    BUILD_URL="${MIRROR}/releases/amd64/autobuilds/current-stage3-amd64"
+    STAGE3_URL="${BUILD_URL}/${STAGE3}"
+    STAGE3_CHECKSUM_URL="${BUILD_URL}/${STAGE3_CHECKSUM}"
 
-        cd stage3
+    cd stage3
 
-        echo "------"
-        echo "DOWNLOADING STAGE3"
+    echo "------"
+    echo "DOWNLOADING STAGE3"
 
 
-        if [[ -f $STAGE3 ]]; then
-                echo "STAGE3 ${STAGE3} ALREADY DOWNLOADED"
-        else
-                wget $STAGE3_URL
-                wget $STAGE3_CHECKSUM_URL
-        fi
+    if [[ -f $STAGE3 ]]; then
+        echo "STAGE3 ${STAGE3} ALREADY DOWNLOADED"
+    else
+        wget $STAGE3_URL
+        wget $STAGE3_CHECKSUM_URL
+    fi
 
-        echo "EXPECTED DIGESTS"
-        sha512sum stage3-$ARCH-$GENTOO_BUILD.tar.bz2
-        echo "ACTUAL DIGESTS"
-        sed -ne '2p' stage3-$ARCH-$GENTOO_BUILD.tar.bz2.DIGESTS
+    echo "EXPECTED DIGESTS"
+    sha512sum stage3-$ARCH-$GENTOO_BUILD.tar.bz2
+    echo "ACTUAL DIGESTS"
+    sed -ne '2p' stage3-$ARCH-$GENTOO_BUILD.tar.bz2.DIGESTS
 
-        echo -n "DIFF: "
-        diff <(sha512sum stage3-$ARCH-$GENTOO_BUILD.tar.bz2) <(sed -ne '2p' stage3-$ARCH-$GENTOO_BUILD.tar.bz2.DIGESTS)
-        if [ $? -eq 0 ]; then
-                echo "OK"
-        else
-                echo "NOT OK"
-                echo "WORLD ON FIRE, RECTIFY THIS SITUATION AND RUN AGAIN."
-                exit 1
-        fi
+    echo -n "DIFF: "
+    diff <(sha512sum stage3-$ARCH-$GENTOO_BUILD.tar.bz2) <(sed -ne '2p' stage3-$ARCH-$GENTOO_BUILD.tar.bz2.DIGESTS)
+    if [ $? -eq 0 ]; then
+        echo "OK"
+    else
+        echo "NOT OK"
+        echo "WORLD ON FIRE, RECTIFY THIS SITUATION AND RUN AGAIN."
+        exit 1
+    fi
 
-        cd ..
+    cd ..
 }
 
-function download_portage_tree() {
+download_portage_tree() {
 
 
-        PORTAGE="portage-latest.tar.bz2"
-        PORTAGE_URL="${MIRROR}/snapshots/${PORTAGE}"
+    PORTAGE="portage-latest.tar.bz2"
+    PORTAGE_URL="${MIRROR}/snapshots/${PORTAGE}"
 
-        cd portage
+    cd portage
 
-        echo "------"
-        echo "DOWNLOADING PORTAGE TREE"
+    echo "------"
+    echo "DOWNLOADING PORTAGE TREE"
 
 
-        if [[ -f $PORTAGE ]]; then
-                echo "PORTAGE TREE ${PORTAGE} ALREADY DOWNLOADED"
-        else
-                wget $PORTAGE_URL
-        fi
+    if [[ -f $PORTAGE ]]; then
+        echo "PORTAGE TREE ${PORTAGE} ALREADY DOWNLOADED"
+    else
+        wget $PORTAGE_URL
+    fi
 
-        cd ..
+    cd ..
 }
 
-function unpack_chroot() {
-        echo "------"
-        echo "POPULATING CHROOT ${builddir}"
-        mkdir -p $builddir
-        cd $builddir
+unpack_chroot() {
+    echo "------"
+    echo "POPULATING CHROOT ${builddir}"
+    mkdir -p $builddir
+    cd $builddir
 
-        if [[ -d usr ]]; then
-                echo "STAGE3 ALREADY UNPACKED"
-        else
-                echo "UNPACKING STAGE3"
-                tar -xjf ../../stage3/$STAGE3
-        fi
+    if [[ -d usr ]]; then
+        echo "STAGE3 ALREADY UNPACKED"
+    else
+        echo "UNPACKING STAGE3"
+        tar -xjf ../../stage3/$STAGE3
+    fi
 
-        if [[ -d usr/portage ]]; then
-                echo "PORTAGE ALREADY UNPACKED"
-        else
-                echo "UNPACKING PORTAGE"
-                tar -xjf ../../portage/$PORTAGE -C usr
-        fi
-        cd $basedir
+    if [[ -d usr/portage ]]; then
+        echo "PORTAGE ALREADY UNPACKED"
+    else
+        echo "UNPACKING PORTAGE"
+        tar -xjf ../../portage/$PORTAGE -C usr
+    fi
+    cd $basedir
 }
 
-function setup_mounts() {
-        cd $builddir
+setup_mounts() {
+    cd $builddir
 
-        echo "------"
-        echo "SETTING UP CHROOT MOUNTS"
-        mount -o bind /dev ./dev
-        mount -t devpts none ./dev/pts
-        mount -t tmpfs none ./dev/shm
-        mount -t proc none ./proc
-        mount -t sysfs none ./sys
+    echo "------"
+    echo "SETTING UP CHROOT MOUNTS"
+    mount -o bind /dev ./dev
+    mount -t devpts none ./dev/pts
+    mount -t tmpfs none ./dev/shm
+    mount -t proc none ./proc
+    mount -t sysfs none ./sys
 
-        cd $basedir
+    cd $basedir
 }
 
-function copy_bootstrapper() {
-        cp bootstrap-sabayon.sh $builddir
+copy_bootstrapper() {
+    cp bootstrap-sabayon.sh $builddir
 }
 
-uuid=$1
+prepare_environment() {
+    if ! [[ -n $BASEDIR ]]; then
+        echo "BASEDIR is unset, exiting."
+    fi
+    CHROOT_DIR=${CHROOT_DIR:-$BASEDIR/chroots}
+}
+
+chroot_name=$1
 shift
 
 basedir=$(dirname $0)
-builddir="${basedir}/chroots/${uuid}"
+builddir="${basedir}/chroots/${chroot_name}"
 
 download_stage3
 download_portage_tree
