111aa7301c4e600fdfe9b3030d2c66fc53b8c365
Adrien Thebo <git@somethingsinistral.net>
2014-05-05 23:10:25 -0700

    reindent

--- bootstrap-sabayon.sh
+++ bootstrap-sabayon.sh
@@ -1,56 +1,56 @@
 #!/bin/bash
 
-function setup_lang() {
-        export LANG=en_US
-        export LANGUAGE=${LANG}
-        export LC_ALL=${LANG}.UTF-8
+setup_lang() {
+    export LANG=en_US
+    export LANGUAGE=${LANG}
+    export LC_ALL=${LANG}.UTF-8
 
-        echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-        locale-gen
+    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+    locale-gen
 }
 
-function setup_system() {
-        ln -sf ../usr/share/zoneinfo/America/Los_Angeles /etc/localtime
-        echo "nameserver 8.8.8.8" > /etc/resolv.conf
+setup_system() {
+    ln -sf ../usr/share/zoneinfo/America/Los_Angeles /etc/localtime
+    echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
-        # sets python 2.7 as default
-        eselect python set 1
-        env-update
-        source /etc/profile
+    # sets python 2.7 as default
+    eselect python set 1
+    env-update
+    source /etc/profile
 }
 
-function prepare_portage() {
+prepare_portage() {
 
-        #emerge --sync
+    #emerge --sync
 
-        echo 'MAKEOPTS="-j5"' >> /etc/portage/make.conf 
+    echo 'MAKEOPTS="-j5"' >> /etc/portage/make.conf 
 
-        mkdir -p /etc/portage/package.keywords
-        mkdir -p /etc/portage/package.use
+    mkdir -p /etc/portage/package.keywords
+    mkdir -p /etc/portage/package.use
 
-        echo "sys-apps/entropy ~amd64" >> /etc/portage/package.keywords/entropy
-        echo "app-admin/equo ~amd64" >> /etc/portage/package.keywords/entropy
+    echo "sys-apps/entropy ~amd64" >> /etc/portage/package.keywords/entropy
+    echo "app-admin/equo ~amd64" >> /etc/portage/package.keywords/entropy
 
-        echo "app-portage/layman git"  > /etc/portage/package.use/layman
+    echo "app-portage/layman git"  > /etc/portage/package.use/layman
 
-        env-update
-        source /etc/profile
+    env-update
+    source /etc/profile
 }
 
-function install_layman() {
-        emerge -v layman
-        layman -a sabayon
-        layman -S
+install_layman() {
+    emerge -v layman
+    layman -a sabayon
+    layman -S
 
-        echo "source /var/lib/layman/make.conf" >> /etc/portage/make.conf
-        env-update
-        source /etc/profile
+    echo "source /var/lib/layman/make.conf" >> /etc/portage/make.conf
+    env-update
+    source /etc/profile
 }
 
-function install_entropy() {
-        emerge -vt app-admin/equo sys-apps/entropy --autounmask-write
-        etc-update --automode -5
-        emerge -vt app-admin/equo sys-apps/entropy
+install_entropy() {
+    emerge -vt app-admin/equo sys-apps/entropy --autounmask-write
+    etc-update --automode -5
+    emerge -vt app-admin/equo sys-apps/entropy
 }
 
 
